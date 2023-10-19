941004b660867b5472998595797138b695bf9eed
VeyselYasar <119116353+VeyselYasar@users.noreply.github.com>
2023-10-20 01:54:51 +0300

    fist commit

---
+++ android/app/src/main/kotlin/com/example/fltdesignapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fltdesignapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/course_screen.dart
@@ -0,0 +1,182 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/videos_section.dart';
+import '../widgets/description_section.dart';
+
+class CourseScreen extends StatefulWidget {
+  String img;
+  CourseScreen(this.img);
+  @override
+  State<CourseScreen> createState() => _CourseScreenState();
+}
+
+class _CourseScreenState extends State<CourseScreen> {
+  bool isVideoSection = true;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        foregroundColor: Colors.black,
+        backgroundColor: Colors.white,
+        elevation: 0,
+        centerTitle: true,
+        title: Text(
+          widget.img,
+          style: TextStyle(fontWeight: FontWeight.bold, letterSpacing: 1),
+        ),
+        actions: [
+          Padding(
+            padding: EdgeInsets.only(right: 10),
+            child: Icon(
+              Icons.notifications,
+              size: 28,
+              color: Color(0xFF674AEF),
+            ),
+          )
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        child: ListView(
+          children: [
+            Container(
+              padding: EdgeInsets.all(5),
+              width: MediaQuery.of(context).size.width,
+              height: 200,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Color(0xFFF5F3FF),
+                image: DecorationImage(
+                  image: AssetImage("images/${widget.img}.png"),
+                ),
+              ),
+              child: Center(
+                  child: Container(
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  shape: BoxShape.circle,
+                ),
+                child: Icon(
+                  Icons.play_arrow_rounded,
+                  color: Color(0xFF674AEF),
+                  size: 35,
+                ),
+              )),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Text(
+              "${widget.img} Complete Course",
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Text(
+              "Created by Dear Programmer",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+                color: Colors.black.withOpacity(0.7),
+              ),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Text(
+              "55 Videos  ",
+              style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.w500,
+                color: Colors.black.withOpacity(0.5),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+              decoration: BoxDecoration(
+                color: Color(0xFFF5F3FF),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Material(
+                    //  if isVideoSection conditon is true then that color will be on botton and
+                    // if conditon is false then that color will be shown on botton but with some
+                    // opacity.
+                    color: isVideoSection
+                        ? Color(0xFF674AEF)
+                        : Color(0xFF674AEF).withOpacity(0.6),
+                    borderRadius: BorderRadius.circular(10),
+                    child: InkWell(
+                      onTap: () {
+                        //change value of isVideosSection
+                        setState(() {
+                          isVideoSection = true;
+
+                        });
+                      },
+                      child: Container(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 15, horizontal: 35),
+                        child: Text(
+                          "Videos",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Material(
+                    color: isVideoSection
+                        ? Color(0xFF674AEF).withOpacity(0.6)
+                        : Color(0xFF674AEF),
+                    borderRadius: BorderRadius.circular(10),
+                   
+                    child: InkWell(
+                      onTap: () {
+                        setState(() {
+                          isVideoSection = false;
+                        });
+                      },
+                      child: Container(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 15, horizontal: 35),
+                        child: Text(
+                          "Description",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                     
+                      
+                    ),
+                  ),
+                ],
+              ),
+            ),
+             // we will create 2 different section for videos section and description section 
+                      SizedBox(height: 10),
+                      // if isVideoSection condition is true then VideoSection will  be 
+                      //shown and if condition is false means DescriptionSection will be shown 
+                      isVideoSection ? VideoSection() : DescriptionSection(), 
+          ],
+        ),
+      ),
+    );
+  }
+}
